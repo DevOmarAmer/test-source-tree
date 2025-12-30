52842eaec191279cec81ae87e28e36faad546a5d
AbdullhGaber <abdullh760@yahoo.com>
2025-12-30 12:15:15 +0200

    Change HelloITI.java

--- HelloITI.java
+++ HelloITI.java
@@ -1,6 +1,6 @@
 public class HelloITIjava {
     public static void main(String[] args) {
-        printITI("Hello, ITI!");
+        printITI("Hello, Abdullh!");
     }
 
     public static void printITI(String str) {
