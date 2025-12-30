7f91a85a49f1a101f3e3ea7ac0b1e9acc199b5dd
Ashraf Sherif <shifoashraf75@gmail.com>
2025-12-30 11:28:48 +0200

    fix java files

--- helloo.java
+++ helloo.java
@@ -3,8 +3,3 @@ class Main {
         System.out.println("Hello, World!");
     }
 }
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
