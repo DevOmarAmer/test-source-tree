3fde0e94da1d8ef9f4315a170f4e9038caf97206
Omar Amer <omahar1907@gmail.com>
2025-12-30 11:09:29 +0200

    update hello file

--- helloo.java
+++ helloo.java
@@ -3,3 +3,8 @@ class Main {
         System.out.println("Hello, World!");
     }
 }
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
